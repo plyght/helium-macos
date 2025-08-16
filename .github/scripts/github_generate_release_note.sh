f105462f509889ce268ade8f968c7437295fe83d
jj <log@riseup.net>
2025-08-16 16:43:45 +0200

    ci: include git log in release notes (#29)

--- .github/scripts/github_generate_release_note.sh
+++ .github/scripts/github_generate_release_note.sh
@@ -16,7 +16,7 @@ _arm64_hash_name="${_base_hash_name}_arm64-macos.dmg.hashes.md"
 _gh_run_href="https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
 
 touch ./github_release_note.md
-printf '## Helium macOS %s\n\n' "${_helium_version}" | tee -a ./github_release_note.md
+printf '## Helium macOS %s\n' "${_helium_version}" | tee -a ./github_release_note.md
 
 if [ -f $_root_dir/announcements.md ]; then
     printf '### Announcements %s\n\n' | tee -a ./github_release_note.md
@@ -31,5 +31,21 @@ fi
 cat $_arm64_hash_name | tee -a ./github_release_note.md
 printf '\n' | tee -a ./github_release_note.md
 cat $_x64_hash_name | tee -a ./github_release_note.md
-printf '\n\n---\n\n' | tee -a ./github_release_note.md
-printf 'See [this GitHub Actions Run](%s) for the [Workflow file](%s/workflow) used as well as the build logs and artifacts\n' "$_gh_run_href" "$_gh_run_href" | tee -a ./github_release_note.md
+
+submodule_commit_at() {
+    git ls-tree "$1" helium-chromium | awk '{print $3}'
+}
+
+last_tag=$(git describe --tags --abbrev=0)
+commit_then=$(submodule_commit_at "$last_tag")
+commit_now=$(submodule_commit_at HEAD)
+
+{
+  printf 'Changes since last build:\n### helium-macos\n```\n'
+  git log --oneline "$last_tag..HEAD"
+  printf '```\n\n### helium-chromium\n```\n'
+  git -C helium-chromium log --oneline "$commit_then..$commit_now"
+  printf '```\n\n---\n\n'
+  printf 'See [this GitHub Actions Run](%s) for the [Workflow file](%s/workflow) used '
+  printf 'as well as the build logs and artifacts\n' "$_gh_run_href" "$_gh_run_href" 
+} | tee -a ./github_release_note.md
