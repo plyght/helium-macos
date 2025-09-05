9b8a11887df6afe6d4cd0946abdab3a3a6431f14
jj <log@riseup.net>
2025-09-05 12:02:52 +0000

    dev.sh: drop homebrew PATH machinery

--- env.sh
+++ env.sh
@@ -6,22 +6,6 @@ if [[ $_arch == "arm64" ]]; then
   _rust_target="aarch64-apple-darwin"
 fi
 
-_x86_64_homebrew_path="/usr/local/opt"
-_arm64_homebrew_path="/opt/homebrew/opt"
-_homebrew_path="$_x86_64_homebrew_path"
-if [[ $_arch == "arm64" ]]; then
-  _homebrew_path="$_arm64_homebrew_path"
-fi
-_clangxx_path="$_homebrew_path/llvm/bin"
-_ninja_path="$_homebrew_path/ninja/bin"
-_python_path="$_homebrew_path/python3/bin"
-
-export PATH="$_clangxx_path:$_ninja_path:$_python_path:$PATH"
-export CXX="$_clangxx_path/clang++"
-export NINJA="$_ninja_path/ninja"
-export LDFLAGS="-L$_homebrew_path/llvm/lib"
-export CPPFLAGS="-I$_homebrew_path/llvm/include"
-
 # Some path variables
 _root_dir=$(dirname $(greadlink -f $0))
 _download_cache="$_root_dir/build/download_cache"
