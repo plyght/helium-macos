4526db52aa398a39367a28907d9351c19812abc6
wukko <me@wukko.me>
2025-11-02 18:52:09 +0600

    ci: prepare xcode 26 no matter the name, refactor (#106)

---
+++ .github/scripts/github_prepare_xcode.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -eux
+
+set -o pipefail
+
+# Sometimes Xcode is 26.0, sometimes 26. We make sure it's usable no matter its name.
+BASE_XCODE_PATH=/Applications/Xcode_26.0.app
+TARGET_XCODE_PATH=/Applications/Xcode_26.app
+
+if [ ! -e "$BASE_XCODE_PATH" ] && [ ! -e "$TARGET_XCODE_PATH" ]; then
+  echo "Failed to find a suitable version of Xcode"
+  exit 1
+fi
+
+if [ -e "$BASE_XCODE_PATH" ] && [ ! -e "$TARGET_XCODE_PATH" ]; then
+  REAL_XCODE_PATH="$(readlink -f "$BASE_XCODE_PATH")"
+  sudo mv "$REAL_XCODE_PATH" "$TARGET_XCODE_PATH"
+fi
+
+# Cleanup any other Xcode versions
+sudo mv "$TARGET_XCODE_PATH" /Applications/tmp_Xcode_26.app
+sudo rm -rf /Applications/Xcode*
+sudo mv /Applications/tmp_Xcode_26.app "$TARGET_XCODE_PATH"
+
+# Switch to target Xcode and clean simulators
+sudo xcode-select --switch "$TARGET_XCODE_PATH"
+sudo xcrun simctl delete all
--- .github/scripts/sanity-check.sh
+++ .github/scripts/sanity-check.sh
@@ -2,7 +2,6 @@
 set -exo pipefail
 
 sudo mdutil -a -i off
-sudo xcode-select --switch /Applications/Xcode_26.0.app
 
 brew install ninja coreutils python@3.14 quilt llvm@20 --overwrite
 brew unlink python || true
