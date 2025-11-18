89e339131f07f0b5a952558d1dd345344923692b
wukko <me@wukko.me>
2025-11-18 16:18:16 +0600

    dev.sh: avoid system permission dialogs when running (#115)
    --use-mock-keychain prevents keychain prompts
    --disable-features=DialMediaRouteProvider prevents local network prompts
    
    chromium build guide recommends using both of these for sake of convenience, and i agree

--- dev.sh
+++ dev.sh
@@ -147,7 +147,9 @@ ___helium_build() {
 ___helium_run() {
     cd "$_src_dir" && ./out/Default/Helium.app/Contents/MacOS/Helium \
     --user-data-dir="$HOME/Library/Application Support/net.imput.helium.dev" \
-    --enable-ui-devtools=$RANDOM
+    --enable-ui-devtools \
+    --use-mock-keychain \
+    --disable-features=DialMediaRouteProvider
 }
 
 ___helium_pull() {
